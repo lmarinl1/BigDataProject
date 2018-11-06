d97fa57b24d30193bb743db35e44c24445e94059
TheBaxes <sebas1475@gmail.com>
2018-11-06 08:08:02 -0500

    Final code version

--- src/spark-npl.scala
+++ src/spark-npl.scala
@@ -8,6 +8,19 @@ val tweets = spark.sql("select text from tweets where lang = 'en'")
 // Transform the DataFrame using the sentiment analysis pipeline from spark-npl
 val result = SentimentPipeline().pretrained().transform(tweets)
 
+// Filter resuls by company (comparison between them that contains both
+// words may overlap)
+val pepsi = result.filter(lower($"text").contains("pepsi"))
+val cola = result.filter(lower($"text").contains("coca-cola"))
+
 // Extract the results from the previous transformation and count by results
-val result2 = result.select(explode($"sentiment"))
-result2.select('col.getItem("result") as "result").groupBy("result").count().show
+val pepsi2 = pepsi.select(explode($"sentiment"))
+val pepsi_result = pepsi2.select('col.getItem("result") as "result").groupBy("result").count()
+
+
+val cola2 = cola.select(explode($"sentiment"))
+val cola_result = cola2.select('col.getItem("result") as "result").groupBy("result").count()
+
+// Create temporal views in Apache Spark to manipulate them later with SparkSQL
+pepsi_result.createOrReplaceTempView("pepsi_results")
+cola_result.createOrReplaceTempView("cola_results")
